9809cd02f86789ceabb33b31ecf8039abbe5d79d
Kazuie Souta <kazuiest@gmail.com>
2011-11-16 14:48:13 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HimapMk4::Application.config.secret_token = '8d62515e70362c58245fad6436652d453d6cb77d5898f18d0c5d37684c8bcd551d7e03444a00e374f5f58db2782d278006ccd95c69c5a25ab10ab56b45fa186f'
